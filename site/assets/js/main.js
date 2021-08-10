128814bc45fbc7654829affc0d95039dfc69378b
Nurgleos <84740774+Nurgleos@users.noreply.github.com>
2021-08-11 00:08:11 +0300

    Add files via upload

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,10 +1,20 @@
 // Open/close form
-document.querySelector('.talk-btn').onclick = (event) => {
-    console.log(event);
-    document.querySelector('.open-form').classList.add('active');
-}
+const btns = document.querySelectorAll('.talk-btn');
+// Скорость забагована, когда нибудь поправлю
+const speedAnimation = 0.8; // sec 
+
+btns.forEach((event) => {
+    let form = document.querySelector('.open-form');
+    event.onclick = () => {
+        form.classList.add('active', 'openAnimation');
+        document.querySelector('.openAnimation').style.animationDuration = speedAnimation;
+        setTimeout(() => form.classList.remove('openAnimation'), speedAnimation * 1000 - 50)
+    };
+});
 
 document.querySelector('.close').onclick = () => {
-    document.querySelector('.open-form').classList.remove('active');
+    document.querySelector('.open-form').classList.add('closeAnimation');
+    document.querySelector('.closeAnimation').style.animationDuration = speedAnimation;
+    setTimeout(() => document.querySelector('.open-form').classList.remove('active', 'closeAnimation'), speedAnimation * 1000 - 50)
 
-}
+};
